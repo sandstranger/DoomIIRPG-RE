664add32c13f14b7c1de3fc6184ce0d9bda2c4bf
sandstranger <sandstranger@gmail.com>
2025-10-28 03:18:52 +0400

    better null checking

--- src/Main.cpp
+++ src/Main.cpp
@@ -157,7 +157,7 @@ void pauseSound() {
 
 bool needToShowScreenControls() {
     CAppContainer *appContainer = CAppContainer::getInstance();
-    if (appContainer == nullptr || appContainer->app == nullptr || appContainer->app->canvas == nullptr){
+    if (!appContainer || !appContainer->app|| !appContainer->app->canvas){
         return true;
     }
     int currentCanvasState = appContainer->app->canvas->state;
